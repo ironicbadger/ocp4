208c12df7444d6ab28198303d47da1993a1e4e2a
IronicBadger <alexktz@gmail.com>
2020-07-31 18:07:32 -0400

    added all node types now

--- terraform/4.5/main.tf
+++ terraform/4.5/main.tf
@@ -1,10 +1,8 @@
-
-
 module "master" {
   source = "../rhcos"
   count  = length(var.master_macs)
 
-  name             = "master${count.index + 1}"
+  name             = "${var.cluster_slug}.master${count.index + 1}"
   ignition         = "1234"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   folder           = "awesomo/redhat/ocp45"
@@ -13,6 +11,69 @@ module "master" {
   network          = data.vsphere_network.network.id
   adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
   mac_address      = var.master_macs[count.index]
+  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
+  template         = data.vsphere_virtual_machine.template.id
+  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  disk_size        = 16
+  memory           = "4096"
+  num_cpu          = "2"
+}
+
+module "worker" {
+  source = "../rhcos"
+  count  = length(var.worker_macs)
+
+  name             = "${var.cluster_slug}.worker${count.index + 1}"
+  ignition         = "1234"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  folder           = "awesomo/redhat/ocp45"
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  datastore        = data.vsphere_datastore.nvme500.id
+  network          = data.vsphere_network.network.id
+  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
+  mac_address      = var.worker_macs[count.index]
+  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
+  template         = data.vsphere_virtual_machine.template.id
+  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  disk_size        = 16
+  memory           = "4096"
+  num_cpu          = "2"
+}
+
+module "bootstrap" {
+  source = "../rhcos"
+  count  = length(var.bootstrap_mac)
+
+  name             = "${var.cluster_slug}.bootstrap"
+  ignition         = "1234"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  folder           = "awesomo/redhat/ocp45"
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  datastore        = data.vsphere_datastore.nvme500.id
+  network          = data.vsphere_network.network.id
+  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
+  mac_address      = var.bootstrap_mac[count.index]
+  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
+  template         = data.vsphere_virtual_machine.template.id
+  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  disk_size        = 16
+  memory           = "4096"
+  num_cpu          = "2"
+}
+
+module "lb" {
+  source = "../rhcos"
+  count  = length(var.lb_mac)
+
+  name             = "${var.cluster_slug}.lb"
+  ignition         = "1234"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  folder           = "awesomo/redhat/ocp45"
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  datastore        = data.vsphere_datastore.nvme500.id
+  network          = data.vsphere_network.network.id
+  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
+  mac_address      = var.lb_mac[count.index]
   datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
   template         = data.vsphere_virtual_machine.template.id
   thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
--- terraform/4.5/variables.tf
+++ terraform/4.5/variables.tf
@@ -31,8 +31,32 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+variable "cluster_slug" {
+    type    = string
+    default = "ocp45"
+}
+
+variable "lb_mac" {
+    description = "OCP 4 HAProxy MAC Address"
+    type        = list(string)
+    default     = ["00:50:56:b1:ef:ac"]
+}
+
+variable "bootstrap_mac" {
+    description = "OCP 4 Bootstrap MAC Address"
+    type        = list(string)
+    default     = ["00:50:56:b1:c7:ae"]
+}
+
 variable "master_macs" {
     description = "OCP 4 Master MAC Address"
     type        = list(string)
     default     = ["00:50:56:b1:c7:ba", "00:50:56:b1:c7:bb", "00:50:56:b1:c7:bc"]
+}
+
+variable "worker_macs" {
+    description = "OCP 4 Worker MAC Address"
+    type        = list(string)
+    #default     = ["00:50:56:b1:c7:ca", "00:50:56:b1:c7:cb"]
+    default     = ["00:50:56:b1:c7:ca", "00:50:56:b1:c7:cb", "00:50:56:b1:c7:cc"]
 }
