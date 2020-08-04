7ddeeedfeac337620295a5f47235e1e745595b96
IronicBadger <alexktz@gmail.com>
2020-08-04 11:24:22 -0400

    restructured node var layout in module definitions

--- terraform/infra/4.5/main.tf
+++ terraform/infra/4.5/main.tf
@@ -3,21 +3,25 @@ module "master" {
   count  = length(var.master_macs)
 
   name             = "${var.cluster_slug}-master${count.index + 1}"
-  ignition         = "1234"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  folder           = "awesomo/redhat/ocp45"
-  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  folder           = "awesomo/redhat/${var.cluster_slug}"
   datastore        = data.vsphere_datastore.nvme500.id
-  network          = data.vsphere_network.network.id
-  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
-  mac_address      = var.master_macs[count.index]
-  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
-  template         = data.vsphere_virtual_machine.template.id
-  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
-  domain_name      = var.domain_name
+
   disk_size        = 40
   memory           = 8192
   num_cpu          = 4
+
+  ignition         = "1234"
+  
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
+  template         = data.vsphere_virtual_machine.template.id
+  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  
+  network          = data.vsphere_network.network.id
+  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
+  mac_address      = var.master_macs[count.index]
+  domain_name      = var.domain_name
 }
 
 module "worker" {
@@ -25,43 +29,51 @@ module "worker" {
   count  = length(var.worker_macs)
 
   name             = "${var.cluster_slug}-worker${count.index + 1}"
-  ignition         = "1234"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  folder           = "awesomo/redhat/ocp45"
-  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  folder           = "awesomo/redhat/${var.cluster_slug}"
   datastore        = data.vsphere_datastore.nvme500.id
-  network          = data.vsphere_network.network.id
-  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
-  mac_address      = var.worker_macs[count.index]
-  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
-  template         = data.vsphere_virtual_machine.template.id
-  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
-  domain_name      = var.domain_name
+
   disk_size        = 40
   memory           = 8192
   num_cpu          = 4
+
+  ignition         = "1234"
+  
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
+  template         = data.vsphere_virtual_machine.template.id
+  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  
+  network          = data.vsphere_network.network.id
+  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
+  mac_address      = var.master_macs[count.index]
+  domain_name      = var.domain_name
 }
 
 module "bootstrap" {
   source = "../../rhcos"
-  count  = length(var.bootstrap_mac)
+  count  = 0
 
   name             = "${var.cluster_slug}-bootstrap"
-  ignition         = "1234"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  folder           = "awesomo/redhat/ocp45"
-  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  folder           = "awesomo/redhat/${var.cluster_slug}"
   datastore        = data.vsphere_datastore.nvme500.id
-  network          = data.vsphere_network.network.id
-  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
-  mac_address      = var.bootstrap_mac[count.index]
-  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
-  template         = data.vsphere_virtual_machine.template.id
-  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
-  domain_name      = var.domain_name
+
   disk_size        = 40
   memory           = 8192
   num_cpu          = 4
+
+  ignition         = "1234"
+  
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
+  template         = data.vsphere_virtual_machine.template.id
+  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  
+  network          = data.vsphere_network.network.id
+  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
+  mac_address      = var.master_macs[count.index]
+  domain_name      = var.domain_name
 }
 
 module "lb" {
@@ -69,19 +81,23 @@ module "lb" {
   count  = length(var.lb_mac)
 
   name             = "${var.cluster_slug}-lb"
-  ignition         = ""
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  folder           = "awesomo/redhat/ocp45"
-  guest_id         = data.vsphere_virtual_machine.rhel7.guest_id
+  folder           = "awesomo/redhat/${var.cluster_slug}"
   datastore        = data.vsphere_datastore.nvme500.id
-  network          = data.vsphere_network.network.id
-  adapter_type     = data.vsphere_virtual_machine.rhel7.network_interface_types[0]
-  mac_address      = var.lb_mac[count.index]
-  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
-  template         = data.vsphere_virtual_machine.rhel7.id
-  thin_provisioned = data.vsphere_virtual_machine.rhel7.disks.0.thin_provisioned
-  domain_name      = var.domain_name
+
   disk_size        = 20
-  memory           = "1024"
-  num_cpu          = "2"
+  memory           = 1024
+  num_cpu          = 2
+
+  ignition         = ""
+  
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  datacenter_id    = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
+  template         = data.vsphere_virtual_machine.template.id
+  thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  
+  network          = data.vsphere_network.network.id
+  adapter_type     = data.vsphere_virtual_machine.template.network_interface_types[0]
+  mac_address      = var.master_macs[count.index]
+  domain_name      = var.domain_name
 }
