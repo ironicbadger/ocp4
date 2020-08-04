f3d1f057cb7540f26fc9429c7ef3b1321cd4e212
IronicBadger <alexktz@gmail.com>
2020-08-04 11:24:42 -0400

    restructured vars

--- terraform/infra/4.5/variables.tf
+++ terraform/infra/4.5/variables.tf
@@ -1,40 +1,5 @@
-provider "vsphere" {
-  user           = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-user"]
-  password       = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-password"]
-  vsphere_server = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-server"]
-
-  # If you have a self-signed cert
-  allow_unverified_ssl = true
-}
-
-data "vsphere_datacenter" "dc" {
-  name = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
-}
-
-data "vsphere_compute_cluster" "cluster" {
-  name          = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-cluster"]
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_network" "network" {
-  name          = "VM Network"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_datastore" "nvme500" {
-  name          = "nvme500"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_virtual_machine" "template" {
-  name          = "rhcos-4.4.3"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_virtual_machine" "rhel7" {
-  name          = "rhel7"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
+###########################
+## OCP Cluster Vars
 
 variable "cluster_slug" {
   type    = string
@@ -69,4 +34,49 @@ variable "worker_macs" {
     type        = list(string)
     default     = ["00:50:56:b1:c7:ca", "00:50:56:b1:c7:cb"]
     #default     = ["00:50:56:b1:c7:ca", "00:50:56:b1:c7:cb", "00:50:56:b1:c7:cc"]
+}
+
+##############
+## VMware templates to clone
+
+data "vsphere_virtual_machine" "template" {
+  name          = "rhcos-4.4.3"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "rhel7" {
+  name          = "rhel7"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+################
+## VMware vars - unlikely to need to change between releases of OCP
+
+
+provider "vsphere" {
+  user           = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-user"]
+  password       = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-password"]
+  vsphere_server = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-server"]
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-dc"]
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = yamldecode(file("~/.config/ocp/vsphere.yaml"))["vsphere-cluster"]
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "nvme500" {
+  name          = "nvme500"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
