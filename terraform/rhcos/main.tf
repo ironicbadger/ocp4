8a2932bae7fb0e86b9a549fa8f910eba0e8c2962
IronicBadger <alexktz@gmail.com>
2020-08-04 22:39:15 -0400

    using extra_config not vapp props to support large ign files

--- terraform/rhcos/main.tf
+++ terraform/rhcos/main.tf
@@ -1,14 +1,14 @@
 resource "vsphere_virtual_machine" "vm" {
-  
-  name                = var.name
-  resource_pool_id    = var.resource_pool_id
-  datastore_id        = var.datastore
-  num_cpus            = var.num_cpu
-  memory              = var.memory
-  memory_reservation  = var.memory
-  guest_id            = var.guest_id
-  folder              = var.folder
-  enable_disk_uuid    = "true"
+
+  name               = var.name
+  resource_pool_id   = var.resource_pool_id
+  datastore_id       = var.datastore
+  num_cpus           = var.num_cpu
+  memory             = var.memory
+  memory_reservation = var.memory
+  guest_id           = var.guest_id
+  folder             = var.folder
+  enable_disk_uuid   = "true"
 
   wait_for_guest_net_timeout  = "0"
   wait_for_guest_net_routable = "false"
@@ -30,11 +30,16 @@ resource "vsphere_virtual_machine" "vm" {
     template_uuid = var.template
   }
 
-  vapp {
-    properties = {
-      "guestinfo.ignition.config.data"          = base64encode(var.ignition)
-      "guestinfo.ignition.config.data.encoding" = "base64"
-    }
+  # vapp {
+  #   properties = {
+  #     "guestinfo.ignition.config.data"          = base64encode(var.ignition)
+  #     "guestinfo.ignition.config.data.encoding" = "base64"
+  #   }
+  # }
+
+  extra_config = {
+    "guestinfo.ignition.config.data"          = base64encode(var.ignition)
+    "guestinfo.ignition.config.data.encoding" = "base64"
   }
 
 }
