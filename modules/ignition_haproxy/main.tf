f0c6de474610a6c7e82c5aaa9394ae44b836caac
IronicBadger <alexktz@gmail.com>
2020-10-21 11:23:05 -0400

    moved files into folder to match coredns module layout

--- modules/ignition_haproxy/main.tf
+++ modules/ignition_haproxy/main.tf
@@ -1,13 +1,13 @@
 data "ignition_systemd_unit" "haproxy" {
   name    = "haproxy.service"
-  content = file("${path.module}/haproxy.service")
+  content = file("${path.module}/files/haproxy.service")
 }
 
 data "ignition_file" "haproxy" {
   path = "/etc/haproxy/haproxy.conf"
   mode = "420" // 0644
   content {
-    content = templatefile("${path.module}/haproxy.tmpl", {
+    content = templatefile("${path.module}/files/haproxy.tmpl", {
       lb_ip_address = var.lb_ip_address,
       api           = var.api_backend_addresses,
       ingress       = var.ingress
